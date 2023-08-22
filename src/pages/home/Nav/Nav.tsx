f04fed1764139fc911ed390c13287bfd61eab8c0
yen <l.yennie.j@gmail.com>
2023-08-23 00:11:38 +0900

    컴포넌트 정리,시멘틱 태그

---
+++ src/pages/home/Nav/Nav.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-scroll";
+
+import ProjectButton from "./ProjectButton";
+
+// react-scroll: Link태그의 to 이름과 태그에 id 가 동일하면 클릭 시 이동
+// 현재 보고있는 페이지를 알려줌. 반응형
+
+const Nav = () => {
+  return (
+    <nav className="fixed top-0 w-full bg-stone-800 z-10 text-slate-600 2xl:flex 2xl:items-center 2xl:right-0 2xl:w-1/5 2xl:h-screen">
+      <ul className="flex w-full text-3xl 2xl:text-6xl 2xl:flex-col">
+        <li className="flex flex-row items-center leading-loose">
+          <Link
+            to="profile"
+            spy={true}
+            smooth={true}
+            duration={300}
+            className="w-full px-6 cursor-pointer hover:text-white "
+            activeClass="bg-stone-900 cursor-default pointer-events-none text-white "
+          >
+            Profile
+          </Link>
+        </li>
+        <li className="flex flex-row items-center leading-loose 2xl:flex-col">
+          <ul className="flex h-full text-2xl 2xl:flex-col 2xl:w-full 2xl:text-4xl">
+            <ProjectButton projectName="YenPin" />
+            <ProjectButton projectName="Portfolio" />
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/pages/home/Nav/ProjectButton.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-scroll";
+
+interface IProjectButton {
+  projectName: string;
+}
+const ProjectButton = ({ projectName }: IProjectButton) => {
+  return (
+    <li className="group flex relative leading-loose">
+      <Link
+        to={projectName}
+        spy={true}
+        smooth={true}
+        duration={100}
+        className="flex items-center w-12 h-full px-3 cursor-pointer transition ease-in delay-150 duration-300 group-hover:bg-stone-900 md:w-full md:justify-center"
+        activeClass="bg-stone-900 cursor-default pointer-events-none [&_div]:bg-white md:[&_div]:opacity-0 [&_span]:opacity-100"
+      >
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-5 h-5 bg-slate-600 rounded-full transition ease-in delay-150 duration-300 group-hover:bg-white md:group-hover:opacity-0  2xl:w-6 2xl:h-6" />
+        <span className="hidden text-white md:inline opacity-0 transition ease-in delay-150 duration-300 group-hover:opacity-100">
+          {projectName}
+        </span>
+      </Link>
+    </li>
+  );
+};
+
+export default ProjectButton;
